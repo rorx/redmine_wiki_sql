c6a16549a1453821a648806b3e977837b8ecd29b
ramalho <rodrigoxavier@gmail.com>
2013-02-20 09:43:50 -0300

    Update init.rb

--- init.rb
+++ init.rb
@@ -12,34 +12,38 @@ Redmine::Plugin.register :redmine_wiki_sql do
   Redmine::WikiFormatting::Macros.register do
     desc "Run SQL query"
     macro :sql do |obj, args, text|
-       
+
         _sentence = args.join(",")
         _sentence = _sentence.gsub("\\(", "(")
         _sentence = _sentence.gsub("\\)", ")")
-        
-        result = ActiveRecord::Base.connection.select_all _sentence
+
+        result = ActiveRecord::Base.connection.execute(_sentence)
         unless result.nil?
-          _line = result[0]
-          unless _line.nil?
+          unless result.num_rows() == 0
+            column_names = []
+            for columns in result.fetch_fields.each do
+              column_names += columns.name.to_a
+            end
+
             _thead = '<tr>'
-            _line.each do |key,value|
-              _thead << '<th>' + key.to_s + '</th>'
+            column_names.each do |column_name|
+              _thead << '<th>' + column_name.to_s + '</th>'
             end
             _thead << '</tr>'
-            
+
             _tbody = ''
-            result.each do |record|
+            result.each_hash do |record|
               unless record.nil?
                 _tbody << '<tr>'
-                record.each do |key,value|
-                  _tbody << '<td>' + textilizable(value.to_s) + '</td>'
+                column_names.each do |column_name|
+                  _tbody << '<td>' + record[column_name].to_s + '</td>'
                 end
                 _tbody << '</tr>'
               end 
             end
-            
+
             text = '<table>' << _thead << _tbody << '</table>' 
-                       
+
             text.html_safe
           else
             ''.html_safe
@@ -47,7 +51,7 @@ Redmine::Plugin.register :redmine_wiki_sql do
         else
           ''.html_safe
         end
-    end	
+    end 
   end
 	
 end
